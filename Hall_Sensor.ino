be54c33287f9d682ba853bb530981c839bdf300d
LeffiLeftism <35105361+LeffiLeftism@users.noreply.github.com>
2021-08-26 17:58:30 +0200

    Works without I2C-Headerfile and without I2C-commands

--- Hall_Sensor.ino
+++ Hall_Sensor.ino
@@ -2,11 +2,11 @@
 
 #include "Arduino.h"
 #include "hall-Sensor.h"
-#include "I2C_functions.h"
+//#include "I2C_functions.h"
 #include <Wire.h>
 
 #define SENSOR_ADDR 0x20
-MbedI2C IC2(4, 5);
+//MbedI2C IC2(4, 5);
 
 #define SensorPin1 2 // Definiert den PIN, an welchem der Hallsensor angeschlossen wird. Dieser muss ein Interruptfähiger PIN sein!
 #define MAGNETS 1    // Definiert die Anzahl der der verwendeten Magneten
@@ -16,11 +16,11 @@ HallSensor rpm_pedal("Pedal-RPM", SensorPin1, 60000 / MAGNETS);
 void setup()
 {
     pinMode(13, OUTPUT);
-    IC2.begin(SENSOR_ADDR);      // join i2c bus with SENSOR_ADDR
-    IC2.onRequest(requestEvent); // register event
+    //IC2.begin(SENSOR_ADDR);      // join i2c bus with SENSOR_ADDR
+    //IC2.onRequest(requestEvent); // register event
 
     pinMode(SensorPin1, INPUT);                                              // Legt den Modus des Pins fest, an welchem der Sensor angeschlossen ist
-    attachInterrupt(digitalPinToInterrupt(SensorPin1), interrupt_func, LOW); // Fügt dem Pin des Sensors eine Interrupt-Funktion hinzu, welche beim festgelegten Status des Pins ausgeführt wird
+    //attachInterrupt(digitalPinToInterrupt(SensorPin1), interrupt_func, LOW); // Fügt dem Pin des Sensors eine Interrupt-Funktion hinzu, welche beim festgelegten Status des Pins ausgeführt wird
 }
 
 void loop()
@@ -53,5 +53,5 @@ void interrupt_func()
 
 void requestEvent()
 {
-    IC2.print(floatToString(rpm_pedal.average_value, 1, 5)); // respond with message of 5 bytes
+    //IC2.print(floatToString(rpm_pedal.average_value, 1, 5)); // respond with message of 5 bytes
 }
